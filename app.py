747f5f54347b5f6fe92d7c8b4799c816bf09422d
VIXIEN-009 <veydatoko@shgcham.school.nz>
2026-06-04 12:59:13 +1200

    query

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask , render_template
+import sqlite3 
 
 app = Flask(__name__)
 
+DATABASE = "database.db"
+
+def query_db(sql,args=(),one=False):
+  '''connect and query- will retun one item if one=true and can accept arguments as tuple'''
+  db = sqlite3.connect(DATABASE)
+  cursor = db.cursor()
+  cursor.execute(sql)
+  cursor.fetchall()
+  db.commit()
+  db.close()
+  return (results[0] if results else None) if one else results
+
+
 @app.route( '/' )
 def index():
-    return render_template('index.html')
+    results = query_db("SELECT * FROM item")  
+    return render_template('index.html',results=results)
 
 @app.route( '/signup' )
 def signup():
@@ -17,6 +32,7 @@ def login():
 @app.route( '/libary' )
 def libary():
     return render_template('libary.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
